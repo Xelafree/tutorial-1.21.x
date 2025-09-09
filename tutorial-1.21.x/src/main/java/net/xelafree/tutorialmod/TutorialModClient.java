2f7eef38d0ba35185063d7d021188b5ecaa9334f
xelafree <xelafree@homosexual.misc.iastate.edu>
2025-09-09 00:29:29 -0500

    Base project

---
+++ tutorial-1.21.x/src/main/java/net/xelafree/tutorialmod/TutorialModClient.java
@@ -0,0 +1,13 @@
+package net.xelafree.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+
+	@Override
+	public void onInitializeClient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
